f55596798a01d9a74eb0c2dc02972f3ac66fcb82
Fianu Dovlo <dovlofianu@gmail.com>
2020-01-24 23:39:18 +0000

    Feature: Close hamburger menu on link click functionality

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ const body = selectElement('body');
 menuToggler.addEventListener('click', () => {
   body.classList.toggle('open');
 });
+
+// Close menu on link click
+const navLink = document.querySelectorAll('.nav-link');
+
+Array.from(navLink).forEach(function(element) {
+  element.addEventListener('click', () => {
+    body.classList.remove('open');
+  })
+})
