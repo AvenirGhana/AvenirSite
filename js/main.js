ad5e289b1446cd8522ba33d15d81d4c950ac0e69
Fianu Dovlo <dovlofianu@gmail.com>
2020-01-29 17:03:20 +0000

    Added scroll magic animation functions

--- js/main.js
+++ js/main.js
@@ -18,3 +18,41 @@ Array.from(navLink).forEach(function(element) {
     body.classList.remove('open');
   })
 })
+
+// Scroll Reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 200
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400
+});
+
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400
+});
+
+sr.reveal('.animate-top-fast', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 200
+});
